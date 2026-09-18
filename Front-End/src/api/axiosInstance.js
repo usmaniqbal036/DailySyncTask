72c2bf1038aa5a4474c3268ff4774c77bd2f9b95
Usman Iqbal <usmaniqbal036@gmail.com>
2026-09-19 00:49:31 +0500

    APiupdated

--- Front-End/src/api/axiosInstance.js
+++ Front-End/src/api/axiosInstance.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api").replace(/\/+$/, "");
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: API_BASE_URL,
 });
-console.log("API base URL:", import.meta.env.VITE_API_BASE_URL);
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
+    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
